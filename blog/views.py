e531d19d9039c06e2509079602ee617e6150bad4
sidjui johann <johannmaxime@yahoo.fr>
2022-10-13 10:07:26 +0100

    premier commit

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+    context_object_name = 'blog_article'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
